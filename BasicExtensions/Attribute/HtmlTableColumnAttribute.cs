d371801c3a93b27bc48568775b0da239124f2e10
mosaduman <53450184+mosaduman@users.noreply.github.com>
2024-11-05 11:44:56 +0300

    Update HtmlTableColumnAttribute.cs

--- BasicExtensions/Attribute/HtmlTableColumnAttribute.cs
+++ BasicExtensions/Attribute/HtmlTableColumnAttribute.cs
@@ -1,19 +1,8 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: BasicExtensions.Attribute.HtmlTableColumnAttribute
-// Assembly: BasicExtensions, Version=22.3.29.850, Culture=neutral, PublicKeyToken=null
-// MVID: 79FB4BFA-4B02-474B-868E-39507878FD3D
-// Assembly location: C:\Users\Musa Duman\.nuget\packages\basic.extensions\22.3.29.850\lib\net5.0\BasicExtensions.dll
-
 namespace BasicExtensions.Attribute
 {
-  public class HtmlTableColumnAttribute : System.Attribute
-  {
-    public HtmlTableColumnAttribute()
+    public class HtmlTableColumnAttribute : System.Attribute
     {
+        public HtmlTableColumnAttribute(string name) => this.PropertyName = name;
+        public string PropertyName { get; set; }
     }
-
-    public HtmlTableColumnAttribute(string name) => this.PropertyName = name;
-
-    public string PropertyName { get; set; }
-  }
 }
