4f20ed704b78559738973eb306a9513e4bca44a1
mosaduman <53450184+mosaduman@users.noreply.github.com>
2024-11-05 11:46:58 +0300

    Update DatetimeFormat.cs

--- BasicExtensions/Models/DatetimeFormat.cs
+++ BasicExtensions/Models/DatetimeFormat.cs
@@ -1,32 +1,26 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: BasicExtensions.Models.DatetimeFormat
-// Assembly: BasicExtensions, Version=22.3.29.850, Culture=neutral, PublicKeyToken=null
-// MVID: 79FB4BFA-4B02-474B-868E-39507878FD3D
-// Assembly location: C:\Users\Musa Duman\.nuget\packages\basic.extensions\22.3.29.850\lib\net5.0\BasicExtensions.dll
-
 namespace BasicExtensions.Models
 {
-  public static class DatetimeFormat
-  {
-    public static class LongDateTimeFormat
+    public static class DatetimeFormat
     {
-      public const string TrFormat1 = "dd.MM.yyyy HH:mm:ss";
-      public const string TrFormat2 = "dd/MM/yyyy HH:mm:ss";
-      public const string DbFormat = "yyyy-MM-dd HH:mm:ss";
-    }
+        public static class LongDateTimeFormat
+        {
+            public const string TrFormat1 = "dd.MM.yyyy HH:mm:ss";
+            public const string TrFormat2 = "dd/MM/yyyy HH:mm:ss";
+            public const string DbFormat = "yyyy-MM-dd HH:mm:ss";
+        }
 
-    public static class ShortDateFormat
-    {
-      public const string TrFormat1 = "dd.MM.yyyy";
-      public const string TrFormat2 = "dd/MM/yyyy";
-      public const string DbFormat = "yyyy-MM-dd";
-    }
+        public static class ShortDateFormat
+        {
+            public const string TrFormat1 = "dd.MM.yyyy";
+            public const string TrFormat2 = "dd/MM/yyyy";
+            public const string DbFormat = "yyyy-MM-dd";
+        }
 
-    public static class TimeFormat
-    {
-      public const string Format1 = "HH:mm";
-      public const string Format2 = "HH:mm:ss";
-      public const string Format3 = "HH:mm:ss.fff";
+        public static class TimeFormat
+        {
+            public const string Format1 = "HH:mm";
+            public const string Format2 = "HH:mm:ss";
+            public const string Format3 = "HH:mm:ss.fff";
+        }
     }
-  }
 }
