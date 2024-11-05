aa90754376c5727aab2208a048c176d6fcf5fa05
mosaduman <53450184+mosaduman@users.noreply.github.com>
2024-11-05 11:51:28 +0300

    Update AssemblyInfo.cs

--- BasicExtensions/AssemblyInfo.cs
+++ BasicExtensions/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyCompany("Oğuzhan ÖZBEK, Nebil Yazılım")]
+[assembly: AssemblyCompany("Musa DUMAN,Oğuzhan ÖZBEK, Nebil Yazılım")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyDescription("It includes basic extensions to make programming easier.")]
 [assembly: AssemblyFileVersion("22.3.29.0850")]
