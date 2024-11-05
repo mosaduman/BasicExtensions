c07e5bdcc7b4ee863213e032cd1750b85c381620
mosaduman <53450184+mosaduman@users.noreply.github.com>
2024-11-05 11:48:06 +0300

    Update PhoneFormat.cs

--- BasicExtensions/Models/PhoneFormat.cs
+++ BasicExtensions/Models/PhoneFormat.cs
@@ -1,22 +1,16 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: BasicExtensions.Models.PhoneFormat
-// Assembly: BasicExtensions, Version=22.3.29.850, Culture=neutral, PublicKeyToken=null
-// MVID: 79FB4BFA-4B02-474B-868E-39507878FD3D
-// Assembly location: C:\Users\Musa Duman\.nuget\packages\basic.extensions\22.3.29.850\lib\net5.0\BasicExtensions.dll
-
 namespace BasicExtensions.Models
 {
-  public static class PhoneFormat
-  {
-    public const string Format1 = "5554443322";
-    public const string Format2 = "05554443322";
-    public const string Format3 = "0 555 444 3322";
-    public const string Format4 = "0 555 444 33 22";
-    public const string Format5 = "0 (555) 444 3322";
-    public const string Format6 = "0 (555) 444 33 22";
-    public const string Format7 = "90 (555) 444 3333";
-    public const string Format8 = "90 (555) 444 33 33";
-    public const string Format9 = "+90 (555) 444 3333";
-    public const string Format10 = "+90 (555) 444 33 33";
-  }
+    public static class PhoneFormat
+    {
+        public const string Format1 = "5554443322";
+        public const string Format2 = "05554443322";
+        public const string Format3 = "0 555 444 3322";
+        public const string Format4 = "0 555 444 33 22";
+        public const string Format5 = "0 (555) 444 3322";
+        public const string Format6 = "0 (555) 444 33 22";
+        public const string Format7 = "90 (555) 444 3333";
+        public const string Format8 = "90 (555) 444 33 33";
+        public const string Format9 = "+90 (555) 444 3333";
+        public const string Format10 = "+90 (555) 444 33 33";
+    }
 }
