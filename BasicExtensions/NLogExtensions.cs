8051df4f51e1bfccd6fd2225ca8807f813fc334e
mosaduman <53450184+mosaduman@users.noreply.github.com>
2024-11-05 11:55:56 +0300

    Update NLogExtensions.cs

--- BasicExtensions/NLogExtensions.cs
+++ BasicExtensions/NLogExtensions.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: BasicExtensions.NLogExtensions
-// Assembly: BasicExtensions, Version=22.3.29.850, Culture=neutral, PublicKeyToken=null
-// MVID: 79FB4BFA-4B02-474B-868E-39507878FD3D
-// Assembly location: C:\Users\Musa Duman\.nuget\packages\basic.extensions\22.3.29.850\lib\net5.0\BasicExtensions.dll
-
 using NLog;
 using NLog.Layouts;
 using System;
