8f820ec6bfd13cea8cebfece21e122d80e1be1a8
mosaduman <53450184+mosaduman@users.noreply.github.com>
2024-11-05 11:47:40 +0300

    Update ListItem.cs

--- BasicExtensions/Models/ListItem.cs
+++ BasicExtensions/Models/ListItem.cs
@@ -1,15 +1,8 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: BasicExtensions.Models.ListItem
-// Assembly: BasicExtensions, Version=22.3.29.850, Culture=neutral, PublicKeyToken=null
-// MVID: 79FB4BFA-4B02-474B-868E-39507878FD3D
-// Assembly location: C:\Users\Musa Duman\.nuget\packages\basic.extensions\22.3.29.850\lib\net5.0\BasicExtensions.dll
-
 namespace BasicExtensions.Models
 {
-  public class ListItem
-  {
-    public string Id { get; set; }
-
-    public string Value { get; set; }
-  }
+    public class ListItem
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
 }
